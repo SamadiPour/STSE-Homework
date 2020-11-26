1f8c5ac91cbed07170973ae858e8fca3e3f5bf70
SamadiPour <samadipoor2@gmail.com>
2020-11-26 18:55:52 +0330

    Add works to do

--- src/phase2/main.py
+++ src/phase2/main.py
@@ -1,7 +1,23 @@
 from src.config import *
 from src.phase1.tweet_database_helper import TweetDatabaseHelper
+from nltk.tokenize import RegexpTokenizer
 
 if __name__ == '__main__':
     database = TweetDatabaseHelper(train_csv)
-    for item in database.get_all():
-        print(item)
+    for item in database.get_all()[:3]:
+        text = item[5]
+        print(text)
+
+        # todo: remove user mention
+        # todo: remove links
+        # todo: remove html tags
+        # todo: remove numbers
+        # todo: make words lowercase
+        # todo: remove extra @ " ٪ " + / ) ( = *
+        # todo: fix typos
+        # todo: remove stop words
+        # todo: Normalization
+
+        tk = RegexpTokenizer('\s+', gaps=True)
+        tokens = tk.tokenize(text)
+        print(tokens)
